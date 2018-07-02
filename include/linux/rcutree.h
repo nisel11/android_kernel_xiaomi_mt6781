5ff43125c49b2064a7c06f219988c8c6d39f61f1
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2018-07-02 14:30:37 -0700

    rcu: Define RCU-sched API in terms of RCU for Tree RCU PREEMPT builds
    Now that RCU-preempt knows about preemption disabling, its implementation
    of synchronize_rcu() works for synchronize_sched(), and likewise for the
    other RCU-sched update-side API members.  This commit therefore confines
    the RCU-sched update-side code to CONFIG_PREEMPT=n builds, and defines
    RCU-sched's update-side API members in terms of those of RCU-preempt.
    
    This means that any given build of the Linux kernel has only one
    update-side flavor of RCU, namely RCU-preempt for CONFIG_PREEMPT=y builds
    and RCU-sched for CONFIG_PREEMPT=n builds.  This in turn means that kernels
    built with CONFIG_RCU_NOCB_CPU=y have only one rcuo kthread per CPU.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Cc: Andi Kleen <ak@linux.intel.com>

--- include/linux/rcutree.h
+++ include/linux/rcutree.h
@@ -45,12 +45,17 @@ static inline void rcu_virt_note_context_switch(int cpu)
 	rcu_note_context_switch(false);
 }
 
+void synchronize_rcu(void);
 static inline void synchronize_rcu_bh(void)
 {
 	synchronize_rcu();
 }
-void synchronize_sched_expedited(void);
 void synchronize_rcu_expedited(void);
+
+static inline void synchronize_sched_expedited(void)
+{
+	synchronize_rcu_expedited();
+}
 
 void kfree_call_rcu(struct rcu_head *head, rcu_callback_t func);
 
